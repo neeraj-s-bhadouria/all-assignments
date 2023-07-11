e6b97925ee77af6de3c9ab35f7cf7b60b727f511
neeraj-s-bhadouria <neerajsb28@gmail.com>
2023-07-12 01:14:35 +0530

    solved time calculation problem

--- week-1/Week-1-assignment-with-tests/01-js/medium/times.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/times.js
@@ -8,5 +8,13 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    var timeBeforeAddingElement = performance.now();
+    var sum= 0
+    for(let i=1; i<=n; i++){
+        sum += i;
+    }
+    var timeAfterAddingElement = performance.now();
+    return (timeAfterAddingElement-timeBeforeAddingElement)/1000;
 }
+
+console.log('time = '+ calculateTime(1000000000))
