8409f6e6fca1e31de6220e562c5b253cd4809784
neeraj-s-bhadouria <neerajsb28@gmail.com>
2023-07-15 20:34:12 +0530

    solved 1-promisify-setTimeout.js

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,5 +2,16 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
-function wait(n) {
+async function wait(n) {
+    return new Promise(resolve => {
+         setTimeout(function(){
+            resolve(`${n/1000} seconds have been passed.`);
+         }, n);
+    })
 }
+
+(async function() {
+    console.log(new Date().toLocaleTimeString('it-IT'));
+    await wait(5000);
+    console.log(new Date().toLocaleTimeString('it-IT'));
+})();
